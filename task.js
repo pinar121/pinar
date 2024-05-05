ff137223f4c98b69d14ad6fc15c3a5510b26662f
pinar121 <silverswarovski4@gmail.com>
2024-05-05 17:32:08 +0400

    first commit

---
+++ task.js
@@ -0,0 +1,13 @@
+ function click1(){
+    let random = Math.floor(Math.random()*80+19)
+    console.log(random)
+ }
+
+ function click2(){
+    let round = Math.floor(Math.random()*70+50)
+    console.log(round)
+ }
+
+ function click3(){
+    console.log("Bu buttona kliklediniz")
+ }
